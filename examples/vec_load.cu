17414d9be28559b24b9e277835712d1b160c2a2c
Your Name <you@example.com>
2024-06-24 12:38:08 +0000

    add vec4

--- examples/vec_load.cu
+++ examples/vec_load.cu
@@ -44,6 +44,30 @@ void device_copy_vector2(int* d_in, int* d_out, int N) {
   device_copy_vector2_kernel<<<blocks, threads>>>(d_in, d_out, N);
 }
 
+__global__ void device_copy_vector4_kernel(int* d_in, int* d_out, int N) {
+  int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  for(int i = idx; i < N/4; i += blockDim.x * gridDim.x) {
+    reinterpret_cast<int4*>(d_out)[i] = reinterpret_cast<int4*>(d_in)[i];
+  }
+
+  // in only one thread, process final elements (if there are any)
+  int remainder = N%4;
+  if (idx==N/4 && remainder!=0) {
+    while(remainder) {
+      int idx = N - remainder--;
+      d_out[idx] = d_in[idx];
+    }
+  }
+}
+
+void device_copy_vector4(int* d_in, int* d_out, int N) {
+  int threads = 128;
+  int blocks = min((N/4 + threads-1) / threads, MAX_BLOCKS);
+
+  device_copy_vector4_kernel<<<blocks, threads>>>(d_in, d_out, N);
+}
+
+
 int main() {
     return 0;
 }
