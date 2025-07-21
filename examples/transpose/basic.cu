e2a480702513a7b6327e2ac18102c9cd92cbc16c
Guoliang He <61049384+hgl71964@users.noreply.github.com>
2025-07-21 12:22:09 +0100

    Update basic.cu

--- examples/transpose/basic.cu
+++ examples/transpose/basic.cu
@@ -1,66 +1,147 @@
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
+
 #include <iostream>
-#include <stdio.h>
+#include <cstdlib>
+#include <cuda_runtime.h>
 
-#define MATRIX_M 1024
-#define MATRIX_N 1024
+int cdiv(int len, int block_size) {
+        return (len + block_size -1)/block_size;
+}
 
-__global__ void transpose_naive(float* input, float* output) {
-    int col_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int row_idx = blockIdx.y * blockDim.y + threadIdx.y;
 
-    if (col_idx < MATRIX_N && row_idx < MATRIX_M) {
+__global__ void transpose(float *in, float *out, int M, int N) {
+    int row = blockIdx.x * blockDim.x + threadIdx.x;
+    int col = blockIdx.y * blockDim.y + threadIdx.y;
 
-        // origin
-        int idx = row_idx * MATRIX_N + col_idx;
+    if (row >= M || col >= N) return;
 
-        // target
-        int trans_idx = col_idx * MATRIX_M + row_idx;
+    out[col*M + row] = in[row*N + col];
+}
 
-        // read is consecutive; write is non-consecutive
-        output[trans_idx] = input[idx];
+template <int BM, int BN, int TM_PER_THR>
+__global__ void transpose_thr_blocking(float *in, float *out, int M, int N) {
+    // assign each thr more work to interleave with idx cost
+    int row_start = blockIdx.x * BM;
+    int col_start = blockIdx.y * BN;
+
+    int row = threadIdx.x/(BN);
+    int col = threadIdx.x%(BN);
+    for (int i = 0; i < TM_PER_THR; ++i) {
+        if (row_start + row + i*BM/TM_PER_THR < M && col_start + col < N) 
+                out[(col_start + col)*M + row_start + row + i*BM/TM_PER_THR] = in[(row_start + row + i*BM/TM_PER_THR)*N + col_start + col];
     }
 }
 
-__global__ void vector_transpose(float* input, float* output)
-{
-    int col_idx = blockIdx.x * blockDim.x + threadIdx.x;
-    int row_idx = blockIdx.y * blockDim.y + threadIdx.y;
+template <int BLOCK_SIZE, int TM_PER_THR>
+__global__ void transpose_shmem(float *in, float *out, int M, int N) {
 
-    // 这里每个线程处理4行，4列，因此需要除以4
-    if (col_idx >= (MATRIX_N >> 2) || row_idx >= (MATRIX_M >> 2)) {
-        return;
+        __shared__ float smem[BLOCK_SIZE][BLOCK_SIZE];
+
+    // assign each thr more work to interleave with idx cost
+    int row_start = blockIdx.x * BLOCK_SIZE;
+    int col_start = blockIdx.y * BLOCK_SIZE;
+
+    int row = threadIdx.x/(BLOCK_SIZE);
+    int col = threadIdx.x%(BLOCK_SIZE);
+    for (int i = 0; i < TM_PER_THR; ++i) {
+        if (row_start + row + i*BLOCK_SIZE/TM_PER_THR < M && col_start + col < N) 
+                smem[row + i*BLOCK_SIZE/TM_PER_THR][col] = in[(row_start + row + i*BLOCK_SIZE/TM_PER_THR)*N + col_start + col];
     }
+    __syncthreads();
 
-    // 找到正确的地址
-    int offset = (row_idx * MATRIX_N + col_idx) << 2; // * 4
-    float4* input_v4 = reinterpret_cast<float4*>(input + offset);
+    // normal copy
+//     for (int i = 0; i < TM_PER_THR; ++i) {
+//         if (row_start + row+i*BLOCK_SIZE/TM_PER_THR < M && col_start + col < N) {
+//                 out[(col_start+col)*M + row_start+row+i*BLOCK_SIZE/TM_PER_THR] = smem[row+i*BLOCK_SIZE/TM_PER_THR][col];
+//         }
+//     }
 
-    // 以向量的方式拿到4 * 4矩阵
-    float4 src_row0 = input_v4[0];
-    float4 src_row1 = input_v4[MATRIX_N >> 2];
-    float4 src_row2 = input_v4[MATRIX_N >> 1];
-    float4 src_row3 = input_v4[(MATRIX_N >> 2) * 3];
-
-
-    // 4 * 4小矩阵转置
-    float4 dst_row0 = make_float4(src_row0.x, src_row1.x, src_row2.x, src_row3.x);
-    float4 dst_row1 = make_float4(src_row0.y, src_row1.y, src_row2.y, src_row3.y);
-    float4 dst_row2 = make_float4(src_row0.z, src_row1.z, src_row2.z, src_row3.z);
-    float4 dst_row3 = make_float4(src_row0.w, src_row1.w, src_row2.w, src_row3.w);
-
-    // 将4行写入
-    offset = (col_idx * MATRIX_M + row_idx) << 2;
-    float4* dst_v4 = reinterpret_cast<float4*>(output + offset);
-    dst_v4[0] = dst_row0;
-    dst_v4[MATRIX_M >> 2] = dst_row1;
-    dst_v4[MATRIX_M >> 1] = dst_row2;
-    dst_v4[(MATRIX_M >> 2) * 3] = dst_row3;
+    for (int i = 0; i < TM_PER_THR; ++i) {
+        int trow = threadIdx.x%BLOCK_SIZE;
+        int tcol = threadIdx.x/BLOCK_SIZE;
+        if (col_start + row + i*BLOCK_SIZE/TM_PER_THR < N && row_start + col < M) {
+                out[(col_start + row + i*BLOCK_SIZE/TM_PER_THR)*M+ row_start + col ] = smem[trow][tcol+i*BLOCK_SIZE/TM_PER_THR];
+        }
+    }
 }
+
 
 int main() {
-    float* input = (float*)malloc(sizeof(float) * MATRIX_M * MATRIX_N);
-    float* output = (float*)malloc(sizeof(float) * MATRIX_M * MATRIX_N);
-    return 0;
+        const int M = 128;
+        const int N = 64;
+
+        float *hc =  (float *) malloc(sizeof(float)*M*N);
+        float *out =  (float *) malloc(sizeof(float)*M*N);
+        float *gold =  (float *) malloc(sizeof(float)*M*N);
+
+        for (int k = 0 ; k < M * N; ++k) {
+            hc[k] = k;
+            out[k] = 0;
+        }
+
+        // row major
+        for (int i = 0; i < M; ++i) {
+                for (int j = 0; j < N; ++j) {
+                    gold[j*M+i] = hc[i*N+j];
+                }
+        }
+
+        // for (int i = 0; i < M; ++i) {
+        //         for (int j = 0; j < N; ++j) {
+        //             std::cout << hc[i*N+j] << ", " << gold[i*N+j] << std::endl;
+        //         }
+        // }
+
+        float *dout;
+        float *dc;
+        cudaMalloc((void**)&dout, sizeof(float)*M*N);
+        cudaMalloc((void**)&dc, sizeof(float)*M*N);
+        cudaMemcpy(dout, out, sizeof(float)*M*N,cudaMemcpyHostToDevice);
+        cudaMemcpy(dc, hc, sizeof(float)*M*N,cudaMemcpyHostToDevice);
+
+        {
+        // const int BM = 16;
+        // const int BN = 32;
+        // dim3 block(BM, BN, 1);
+        // dim3 grid(cdiv(M, BM), cdiv(N, BN), 1);
+        // std::cout << "grid: " << grid.x << " " << grid.y << " " << grid.z << std::endl;
+        // std::cout << "block: " << block.x << " " << block.y << " " << block.z << std::endl;
+        // transpose<<<grid, block>>>(dc, dout, M, N);
+        }
+
+
+        {
+        // const int BM = 64;
+        // const int BN = 32;
+        // const int TM_PER_THR = 8;
+        // dim3 grid(cdiv(M, BM), cdiv(N, BN), 1);
+        // dim3 block(BM/TM_PER_THR * BN, 1);
+        // std::cout << "grid: " << grid.x << " " << grid.y << " " << grid.z << std::endl;
+        // std::cout << "block: " << block.x << " " << block.y << " " << block.z << std::endl;
+        // transpose_thr_blocking<BM, BN, TM_PER_THR><<<grid, block>>>(dc, dout, M, N);
+        }
+
+
+        {
+        const int BLOCK = 32;
+        const int TM_PER_THR = 4;
+        dim3 grid(cdiv(M, BLOCK), cdiv(N, BLOCK), 1);
+        dim3 block(BLOCK/TM_PER_THR * BLOCK, 1);
+        std::cout << "grid: " << grid.x << " " << grid.y << " " << grid.z << std::endl;
+        std::cout << "block: " << block.x << " " << block.y << " " << block.z << std::endl;
+        transpose_shmem<BLOCK, TM_PER_THR><<<grid, block>>>(dc, dout, M, N);
+        }
+
+
+        cudaMemcpy(out, dout, sizeof(float)*M*N,cudaMemcpyDeviceToHost);
+        cudaDeviceSynchronize();
+
+        int err = 0;
+        for (int i = 0; i < M; ++i) {
+                for (int j = 0; j < N; ++j) {
+                        if (std::abs(gold[i*N + j] - out[i*N + j]) > 1e-4 ) {
+                                err++;
+                        }
+                }
+        }
+        std::cout << "Err: " << err << std::endl;
 }
